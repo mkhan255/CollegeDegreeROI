64d1566f8a8cf82090cc03c324db5dcdb9b42668
mkhan255 <60115068+mkhan255@users.noreply.github.com>
2020-03-05 12:05:07 -0500

    Update __init__.py

--- spark/__init__.py
+++ spark/__init__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
+
 from os.path import abspath
 from configparser import ConfigParser
 from pyspark.sql import SparkSession
 
-# -- Load Configuration --
+
 config = ConfigParser()
 config.read(abspath('config.ini'))
 jar_path = config.get('Jar', 'jar_path')
+#load configuration file
 
-# -- Init Spark --
+
 spark = SparkSession.builder.appName('college_roi').config("spark.jars", "{}/postgresql-42.2.9.jar".format(jar_path)).getOrCreate()
-
+#initialize spark
