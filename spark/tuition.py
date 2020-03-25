e8e6ddb7e8afc517b4c917ab606ade27467b421a
mkhan255 <60115068+mkhan255@users.noreply.github.com>
2020-03-25 10:36:52 -0400

    Update tuition.py

--- spark/tuition.py
+++ spark/tuition.py
@@ -13,33 +13,31 @@ s3 = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 #uses boto3 to read from AWS S3
 
-def save_tuition()
-#function that retrieves college tution csv from AWS S3 and saves it into Postgresql database
 
-        ipeds_bucket = config.get('AWS', 'ipeds_bucket')
-        tuition_csv = config.get('AWS', 'tuition_csv')
-        #retrieve data from S3 bucket
+ipeds_bucket = config.get('AWS', 'ipeds_bucket')
+tuition_csv = config.get('AWS', 'tuition_csv')
+#retrieve data from S3 bucket
         
-        tuition_df = spark.read.csv("s3a://{}/{}".format(ipeds_bucket, tuition_csv) header='true', inferSchema='true')
-        #read the csv file and store it in a dataframe
+tuition_df = spark.read.csv("s3a://{}/{}".format(ipeds_bucket, tuition_csv) header='true', inferSchema='true')
+#read the csv file and store it in a dataframe
         
-        new_column_name_list = ["unit_id",
-                                "name",
-                                "in-district living on campus", 
-                                "in-state living on campus", 
-                                "out-of-state living on campus", 
-                                "in-district living off campus", 
-                                "in-state living off campus", 
-                                "out-of-state living off campus", 
-                                "in-district living with family", 
-                                "in-state living with family", 
-                                "out-of-state living with family",
-                                "blank"]
-        #re-write the colmun names into a more front-end friendly format
+new_column_name_list = ["unit_id",
+                       "name",
+                        "in-district living on campus", 
+                        "in-state living on campus", 
+                        "out-of-state living on campus", 
+                        "in-district living off campus", 
+                        "in-state living off campus", 
+                        "out-of-state living off campus", 
+                        "in-district living with family", 
+                        "in-state living with family", 
+                        "out-of-state living with family",
+                        "blank"]
+#re-write the colmun names into a more front-end friendly format
 
-        tuition_data = tuition_df.toDF(*new_column_name_list).drop("blank")
-        #save the dataframe with the new column names 
+tuition_data = tuition_df.toDF(*new_column_name_list).drop("blank")
+#save the dataframe with the new column names 
 
-        db = db_connector()
-        db.save(tuition_data, table='tuition')
-        #call the db_connector function and save the dataframe to the database in table named tuition
+db = db_connector()
+db.save(tuition_data, table='tuition')
+#call the db_connector function and save the dataframe to the database in table named tuition
